53f370d1a12579e32e84443e6e152944ebebbf6e
dianaguu <dianaguu@gmail.com>
2022-11-05 22:58:34 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+  console.log('[DIANA app.js] process.env.NODE_ENV:', process.env.NODE_ENV)
+}
+
+const routes = require('./routes');
+
+// express object
+const app = express();
+const PORT = process.env.PORT;
+// ===========================================================
+// views: express-handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+// ===========================================================
+// Invoke Primary Router
+app.use(routes);
+// start server
+app.listen(PORT, () => {
+  console.log(`App is listening on http://localhost:${PORT}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+// /routes/modules/index.js
+const express = require('express');
+const router = express.Router();
+
+const home = require('./modules/home');
+router.use('/', home);
+
+module.exports = router;
