4cc17c8cdf68b72f1d8c10869ae01f52ed03af2c
dianaguu <dianaguu@gmail.com>
2022-11-06 14:54:57 +0800

    feat: add search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,11 +4,35 @@ const router = express.Router();
 
 const Restaurant = require("../../models/restaurant")
 
+// ===========================================================
+// CRUD.
 // Read: view all restaurants
 router.get("/", (req, res) => {
   Restaurant.find({})
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
+    .catch(err => console.log(err))
+})
+
+// Update: search restaurant by keywords (without sort)
+router.get("/search", (req, res) => {
+  // console.log('[DIANA home.js] req.query:', req.query)
+  const keywords = req.query.keywords
+  if (!keywords) {
+    return res.redirect("/")
+  }
+
+  const keyword = req.query.keywords.trim().toLowerCase()
+  Restaurant.find({})
+    .lean()
+    .then(restaurants => {
+      const filterRestaurantsData = restaurants.filter(
+        data =>
+          data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword)
+      )
+      res.render("index", { restaurants: filterRestaurantsData })
+    })
     .catch(err => console.log(err))
 })
 
