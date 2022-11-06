3cddf0bcdbdceb93b31fa1037392a27ce0d69881
dianaguu <dianaguu@gmail.com>
2022-11-06 16:08:47 +0800

    feat: add show feature for detail

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -13,10 +13,23 @@ router.get('/new', (req, res) => {
 
 // Create: create a restaurant
 router.post("/", (req, res) => {
-  console.log('[DIANA restaurant.js] req.body:', req.body)
+  // console.log('[DIANA restaurant.js] req.body:', req.body)
   Restaurant.create(req.body)
     .then(() => res.redirect("/"))
     .catch(err => console.log(err))
 })
 
 module.exports = router
+
+// ===========================================================
+// Read - Show page for specific restaurant
+router.get('/:id', (req, res) => {
+  // console.log('[DIANA restaurant.js] req.params:', req.params)
+  const restaurantId = req.params.id
+  return Restaurant.findById(restaurantId)
+    .lean()
+    .then(restaurant => {
+      console.log('[DIANA restaurant.js] restaurant:', restaurant)
+      res.render("show", { restaurant })})
+    .catch(err => console.log(err))
+})
