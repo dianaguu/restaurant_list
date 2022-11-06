655b1b73c89f351147fcf8b9b27ddc496bb457f0
dianaguu <dianaguu@gmail.com>
2022-11-06 10:07:48 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const routes = require('./routes');
+require('./configs/mongoose');
 
 // express object
 const app = express();
---
+++ configs/mongoose.js
@@ -0,0 +1,17 @@
+// /configs/mongoose.js
+// connect to DB: mongoose
+const mongoose = require('mongoose');
+
+console.log('process.env.MONGODB_URI: ', process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;  // create mongoose.connect() object
+
+db.on('error', () => {  // listen for "error" event
+  console.log('[DIANA mongoose.js] MongoDB error!');
+});
+db.once('open', () => { // listen for "open" event one-time
+  console.log('[DIANA mongoose.js] MongoDB connected!');
+});
+
+module.exports = db
