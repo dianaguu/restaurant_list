addaea1f2a8b3769c3d5834b33b9ad0018f72709
dianaguu <dianaguu@gmail.com>
2022-11-06 17:37:26 +0800

    feat: add user register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,9 +3,16 @@ const express = require('express')
 const router = express.Router()
 
 // ===========================================================
+// CRUD
 // Read - Login page
 router.get('/login', (req, res) => {
   return res.render('login')
+})
+
+// ===========================================================
+// Read - Register page
+router.get('/register', (req, res) => {
+  return res.render('register');
 })
 
 module.exports = router
